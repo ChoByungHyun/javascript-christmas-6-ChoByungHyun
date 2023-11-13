b773fb1062afe47542cd5785a4d0c65eef20dfc8
ChoByungHyun <cbj973@gmail.com>
2023-11-14 07:36:28 +0900

    feat: controller validate 로직 추가

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,6 +1,7 @@
-import { GIFT } from "../constant/menuConstant.js";
+import { MENU } from "../constant/menuConstant.js";
 import { DISCOUNT_MESSAGE } from "../constant/messageConstant.js";
 import EventDomain from "../model/EventDomain.js";
+import Validator from "../model/Validator.js";
 import createDateObject from "../model/createDateObject.js";
 import InputView from "../view/InputView.js";
 import OutputView from "../view/OutputView.js";
@@ -8,12 +9,15 @@ class Controller {
   constructor() {
     this.eventModel = new EventDomain();
     this.createDateObject = new createDateObject();
+    this.validator = new Validator();
   }
 
   async run() {
     const eventCalendar = this.createDateObject.getDiscountDays();
     const visitDate = await this.getVisitDate();
+
     const orderMenu = await this.getMenu();
+
     const totalPrice = this.eventModel.calculateTotalPrice(orderMenu);
     let { totalDiscountAmount, discountDetails } = this.getTotalDiscountDetail(
       eventCalendar,
@@ -23,7 +27,7 @@ class Controller {
     const isGift = this.eventModel.isGift(totalPrice);
     const finalPrice = totalPrice + totalDiscountAmount;
 
-    this.printOrderInformation(orderMenu, totalPrice);
+    this.printOrderInformation(orderMenu, totalPrice, visitDate);
     totalDiscountAmount = this.applyGiftDiscount(
       isGift,
       discountDetails,
@@ -36,11 +40,42 @@ class Controller {
 
   async getVisitDate() {
     OutputView.printWelcomeMessage();
-    return await InputView.readDate();
+    let visitDate;
+    let isValid = false;
+
+    do {
+      try {
+        visitDate = await InputView.readDate();
+        isValid = this.validator.isValidDate(visitDate);
+      } catch (error) {
+        OutputView.printError(error);
+      }
+    } while (!isValid);
+
+    return visitDate;
   }
 
-  printOrderInformation(orderMenu, totalPrice) {
-    OutputView.printOrderMessage();
+  async getMenu() {
+    let orderMenuInput;
+    let orderMenu;
+    let isValid = false;
+
+    do {
+      try {
+        orderMenuInput = await InputView.readMenu();
+        orderMenu = this.eventModel.parseOrderMenu(orderMenuInput);
+
+        isValid = this.validator.isValidMenu(orderMenu);
+      } catch (error) {
+        OutputView.printError(error);
+      }
+    } while (!isValid);
+
+    return orderMenu;
+  }
+
+  printOrderInformation(orderMenu, totalPrice, visitDate) {
+    OutputView.printOrderMessage(visitDate);
     OutputView.printMenu(orderMenu);
     OutputView.printTotalPriceBeforeDiscount(totalPrice);
   }
@@ -52,8 +87,8 @@ class Controller {
   applyGiftDiscount(isGift, discountDetails, totalDiscountAmount) {
     OutputView.printGift(isGift);
     if (isGift) {
-      discountDetails[DISCOUNT_MESSAGE.GIFT] = -GIFT.CHAMPAGNE;
-      totalDiscountAmount -= GIFT.CHAMPAGNE;
+      discountDetails[DISCOUNT_MESSAGE.GIFT] = -MENU.DRINK.샴페인;
+      totalDiscountAmount -= MENU.DRINK.샴페인;
     }
     return totalDiscountAmount;
   }
@@ -62,12 +97,6 @@ class Controller {
     OutputView.printTotalDiscountAmount(totalDiscountAmount);
     OutputView.printTotalPriceAfterDiscount(finalPrice);
     OutputView.printBadge(badge);
-  }
-
-  async getMenu() {
-    const orderMenuInput = await InputView.readMenu();
-    const orderMenu = this.eventModel.parseOrderMenu(orderMenuInput);
-    return orderMenu;
   }
 
   getTotalDiscountDetail(eventCalendar, visitDate, orderMenu) {
