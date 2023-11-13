5f2aa672f3811966e26c162db76351e64db5499f
ChoByungHyun <cbj973@gmail.com>
2023-11-14 05:52:39 +0900

    feat: contorller 기능 구현 및 함수 분리

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -2,41 +2,77 @@ import EventDomain from "../model/EventDomain.js";
 import createDateObject from "../model/createDateObject.js";
 import InputView from "../view/InputView.js";
 import OutputView from "../view/OutputView.js";
-
-class Contorller {
+class Controller {
   constructor() {
     this.eventModel = new EventDomain();
     this.createDateObject = new createDateObject();
   }
+
   async run() {
     const eventCalendar = this.createDateObject.getDiscountDays();
-    OutputView.printWelcomeMessage();
-    const visitDate = await InputView.readDate();
+    const visitDate = await this.getVisitDate();
     const orderMenu = await this.getMenu();
-
     const totalPrice = this.eventModel.calculateTotalPrice(orderMenu);
-    const totalDiscountAmount = this.eventModel.calculateDiscount(
+    let { totalDiscountAmount, discountDetails } = this.getTotalDiscountDetail(
       eventCalendar,
       visitDate,
       orderMenu
     );
+    const isGift = this.eventModel.isGift(totalPrice);
+    const finalPrice = totalPrice + totalDiscountAmount;
 
-    const finalPrice = totalPrice - totalDiscountAmount;
+    this.printOrderInformation(orderMenu, totalPrice);
+    totalDiscountAmount = this.applyGiftDiscount(
+      isGift,
+      discountDetails,
+      totalDiscountAmount
+    );
+    this.printDiscountDetail(discountDetails);
+    const badge = this.eventModel.getBadge(totalDiscountAmount);
+    this.printFinalPriceInformation(totalDiscountAmount, finalPrice, badge);
+  }
 
+  async getVisitDate() {
+    OutputView.printWelcomeMessage();
+    return await InputView.readDate();
+  }
+
+  printOrderInformation(orderMenu, totalPrice) {
     OutputView.printOrderMessage();
     OutputView.printMenu(orderMenu);
     OutputView.printTotalPriceBeforeDiscount(totalPrice);
-    OutputView.printGift();
-    OutputView.printDiscountDetail();
+  }
+
+  printDiscountDetail(discountDetails) {
+    OutputView.printDiscountDetail(discountDetails);
+  }
+
+  applyGiftDiscount(isGift, discountDetails, totalDiscountAmount) {
+    OutputView.printGift(isGift);
+    if (isGift) {
+      discountDetails["증정 이벤트"] = -25000;
+      totalDiscountAmount -= 25000;
+    }
+    return totalDiscountAmount;
+  }
+
+  printFinalPriceInformation(totalDiscountAmount, finalPrice, badge) {
     OutputView.printTotalDiscountAmount(totalDiscountAmount);
     OutputView.printTotalPriceAfterDiscount(finalPrice);
-    OutputView.printBadge();
+    OutputView.printBadge(badge);
   }
+
   async getMenu() {
     const orderMenuInput = await InputView.readMenu();
     const orderMenu = this.eventModel.parseOrderMenu(orderMenuInput);
-
     return orderMenu;
   }
+
+  getTotalDiscountDetail(eventCalendar, visitDate, orderMenu) {
+    let { totalDiscountAmount, discountDetails } =
+      this.eventModel.calculateDiscount(eventCalendar, visitDate, orderMenu);
+    return { totalDiscountAmount: -totalDiscountAmount, discountDetails };
+  }
 }
-export default Contorller;
+
+export default Controller;
