030166973948a732563c1124f0b66bbb313eab64
ChoByungHyun <cbj973@gmail.com>
2023-11-14 05:51:08 +0900

    refactor: 메세지 상수 추가

--- src/constant/messageConstant.js
+++ src/constant/messageConstant.js
@@ -3,11 +3,13 @@ export const OUTPUT_MESSAGE = {
   ORDER: "12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!",
   MENU: "\n<주문 메뉴>",
   BEFORE_DISCOUNT: "\n<할인 전 총주문 금액>",
-  GIFT: "\n<증정 메뉴>",
+  GIFT_MENU: "\n<증정 메뉴>",
   BADGE: "\n<12월 이벤트 배지>",
   DISCOUNT_DETAIL: "\n<혜택 내역>",
   TOTAL_DISCOUNT: "\n<총혜택 금액>",
   AFTER_DISCOUNT: "\n<할인 후 예상 결제 금액>",
+  NOTHING: "없음",
+  GIFT: "샴페인 1개",
 };
 
 export const INPUT_MESSAGE = {
