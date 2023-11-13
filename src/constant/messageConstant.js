af488cb66e68f0c7a9503b447a79ae6535d56e1f
ChoByungHyun <cbj973@gmail.com>
2023-11-14 04:20:23 +0900

    refactor: View 메시지 상수 변경

--- src/constant/messageConstant.js
+++ src/constant/messageConstant.js
@@ -1,4 +1,4 @@
-export const messageConstant = {
+export const OUTPUT_MESSAGE = {
   WELCOME: "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.",
   ORDER: "12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!",
   MENU: "\n<주문 메뉴>",
@@ -8,4 +8,10 @@ export const messageConstant = {
   DISCOUNT_DETAIL: "\n<혜택 내역>",
   TOTAL_DISCOUNT: "\n<총혜택 금액>",
   AFTER_DISCOUNT: "\n<할인 후 예상 결제 금액>",
+};
+
+export const INPUT_MESSAGE = {
+  DATE: "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n",
+  ORDER:
+    "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)",
 };
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,45 +1,43 @@
 import { Console } from "@woowacourse/mission-utils";
-import { messageConstant } from "../constant/messageConstant";
+import { OUTPUT_MESSAGE } from "../constant/OUTPUT_MESSAGE";
 const OutputView = {
   printWelcomeMessage() {
-    Console.print(messageConstant.WELCOME);
+    Console.print(OUTPUT_MESSAGE.WELCOME);
   },
   printOrderMessage() {
-    Console.print(messageConstant.ORDER);
+    Console.print(OUTPUT_MESSAGE.ORDER);
   },
   printMenu(menu) {
-    Console.print(messageConstant.MENU);
+    Console.print(OUTPUT_MESSAGE.MENU);
     for (let item in menu) {
       Console.print(`${item} ${menu[item]}개`);
     }
   },
   printTotalPriceBeforeDiscount(totalPriceBeforeDiscount) {
     Console.print(
-      `${messageConstant.BEFORE_DISCOUNT}\n${totalPriceBeforeDiscount}원`
+      `${OUTPUT_MESSAGE.BEFORE_DISCOUNT}\n${totalPriceBeforeDiscount}원`
     );
   },
   printGift(gift) {
-    Console.print(`${messageConstant.GIFT}\n${gift}`);
+    Console.print(`${OUTPUT_MESSAGE.GIFT}\n${gift}`);
   },
 
   printDiscountDetail(discountDetail) {
-    Console.print(`${messageConstant.DISCOUNT_DETAIL}\n${discountDetail}`);
+    Console.print(`${OUTPUT_MESSAGE.DISCOUNT_DETAIL}\n${discountDetail}`);
   },
 
   printTotalDiscountAmount(totalDiscountAmount) {
-    Console.print(
-      `${messageConstant.TOTAL_DISCOUNT}\n${totalDiscountAmount}원`
-    );
+    Console.print(`${OUTPUT_MESSAGE.TOTAL_DISCOUNT}\n${totalDiscountAmount}원`);
   },
 
   printTotalPriceAfterDiscount(totalPriceAfterDiscount) {
     Console.print(
-      `${messageConstant.AFTER_DISCOUNT}\n${totalPriceAfterDiscount}원`
+      `${OUTPUT_MESSAGE.AFTER_DISCOUNT}\n${totalPriceAfterDiscount}원`
     );
   },
 
   printBadge(badge) {
-    Console.print(`${messageConstant.BADGE}\n${badge}`);
+    Console.print(`${OUTPUT_MESSAGE.BADGE}\n${badge}`);
   },
 };
 export default OutputView;
