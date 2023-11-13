e1243c26da7d5999e6ffe101566d1480e2ff5a18
ChoByungHyun <cbj973@gmail.com>
2023-11-14 07:33:53 +0900

    refactor: domain 로직 추상화

--- src/model/EventDomain.js
+++ src/model/EventDomain.js
@@ -1,4 +1,6 @@
-import { MENU } from "../constant/menuConstant.js";
+import { DATE_CONFIG } from "../constant/dateConstant.js";
+import { DISCOUNT_CONFIG } from "../constant/discountConstant.js";
+import { BADGE, MENU, MENU_TYPE } from "../constant/menuConstant.js";
 import { DISCOUNT_MESSAGE } from "../constant/messageConstant.js";
 
 class EventDomain {
@@ -13,50 +15,60 @@ class EventDomain {
   }
 
   calculateChristmasDDayDiscount(visitDate) {
-    if (visitDate < 1 || visitDate > 25) {
+    if (
+      visitDate < DATE_CONFIG.D_DAY_START ||
+      visitDate > DATE_CONFIG.D_DAY_END
+    ) {
       return 0;
     }
-    return 1000 + (visitDate - 1) * 100;
+    return (
+      DISCOUNT_CONFIG.BASE_DAY_ACCOUNT +
+      (visitDate - 1) * DISCOUNT_CONFIG.TERM_DAY_ACCOUNT
+    );
   }
 
   calculateDiscount(eventCalendar, visitDate, orderMenu) {
     const discountType = eventCalendar[visitDate];
     let totalDiscountAmount = 0;
     let discountDetails = {};
+    const totalPrice = this.calculateTotalPrice(orderMenu);
+    if (totalPrice >= DISCOUNT_CONFIG.MIN_AMOUNT) {
+      const christmasDiscount = this.calculateChristmasDDayDiscount(visitDate);
+      totalDiscountAmount += christmasDiscount;
+      if (christmasDiscount !== 0) {
+        totalDiscountAmount += christmasDiscount;
+        discountDetails[DISCOUNT_MESSAGE.D_DAY] = -christmasDiscount;
+      }
 
-    const christmasDiscount = this.calculateChristmasDDayDiscount(visitDate);
-    totalDiscountAmount += christmasDiscount;
-    discountDetails[DISCOUNT_MESSAGE.D_DAY] = -christmasDiscount;
+      for (let item in orderMenu) {
+        const quantity = orderMenu[item];
 
-    for (let item in orderMenu) {
-      const price = this.getPrice(item);
-      const quantity = orderMenu[item];
+        if (discountType.weekdayDiscount && this.isDessert(item)) {
+          const weekdayDiscount = quantity * DISCOUNT_CONFIG.DAY_DISCOUNT; // 평일 할인액 2,023원 (디저트 메뉴)
+          totalDiscountAmount += weekdayDiscount;
+          discountDetails[DISCOUNT_MESSAGE.WEEK] = -weekdayDiscount;
+        } else if (discountType.weekendDiscount && this.isMain(item)) {
+          const weekendDiscount = quantity * DISCOUNT_CONFIG.DAY_DISCOUNT; // 주말 할인액 2,023원 (메인 메뉴)
+          totalDiscountAmount += weekendDiscount;
+          discountDetails[DISCOUNT_MESSAGE.WEEKEND] = -weekendDiscount;
+        }
+      }
 
-      if (discountType.weekdayDiscount && this.isDessert(item)) {
-        const weekdayDiscount = quantity * 2023; // 평일 할인액 2,023원 (디저트 메뉴)
-        totalDiscountAmount += weekdayDiscount;
-        discountDetails[DISCOUNT_MESSAGE.WEEK] = -weekdayDiscount;
-      } else if (discountType.weekendDiscount && this.isMain(item)) {
-        const weekendDiscount = quantity * 2023; // 주말 할인액 2,023원 (메인 메뉴)
-        totalDiscountAmount += weekendDiscount;
-        discountDetails[DISCOUNT_MESSAGE.WEEKEND] = -weekendDiscount;
+      if (discountType.specialDiscount) {
+        totalDiscountAmount += DISCOUNT_CONFIG.SPECIAL_DISCOUNT; // 특별 할인액 1,000원
+        discountDetails[DISCOUNT_MESSAGE.SPECIAL] =
+          -DISCOUNT_CONFIG.SPECIAL_DISCOUNT;
       }
     }
-
-    if (discountType.specialDiscount) {
-      totalDiscountAmount += 1000; // 특별 할인액 1,000원
-      discountDetails[DISCOUNT_MESSAGE.SPECIAL] = -1000;
-    }
-
     return { totalDiscountAmount, discountDetails };
   }
 
   isDessert(item) {
-    return item in MENU["DESSERT"];
+    return item in MENU[MENU_TYPE.DESSERT];
   }
 
   isMain(item) {
-    return item in MENU["MAIN"];
+    return item in MENU[MENU_TYPE.MAIN];
   }
 
   getPrice(itemName) {
@@ -65,7 +77,6 @@ class EventDomain {
         return MENU[category][itemName];
       }
     }
-    return 0; // 메뉴에 없는 아이템인 경우 0 반환
   }
 
   calculateTotalPrice(orderMenu) {
@@ -81,19 +92,19 @@ class EventDomain {
   }
 
   isGift(totalPrice) {
-    if (totalPrice >= 120000) {
+    if (totalPrice >= DISCOUNT_CONFIG.GIFT_AMOUNT) {
       return true;
     }
     return false;
   }
   getBadge(totalDiscountAmount) {
     totalDiscountAmount = totalDiscountAmount * -1;
-    if (totalDiscountAmount >= 20000) {
-      return "산타";
-    } else if (totalDiscountAmount >= 10000) {
-      return "트리";
-    } else if (totalDiscountAmount >= 5000) {
-      return "별";
+    if (totalDiscountAmount >= BADGE.GOLD_AMOUNT) {
+      return BADGE.GOLD;
+    } else if (totalDiscountAmount >= BADGE.SILVER_AMOUNT) {
+      return BADGE.SILVER;
+    } else if (totalDiscountAmount >= BADGE.BRONZE_AMOUNT) {
+      return BADGE.BRONZE;
     } else {
       return "";
     }
